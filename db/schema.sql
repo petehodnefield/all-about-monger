0c0c74fa4ba82dc66b9bca4004b08df1c4c7a374
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-06-01 10:56:49 -0500

    MVC layout done

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS all_about_monger;
+
+CREATE DATABASE all_about_monger;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const sequelize = require('./config/connection')
+const path = require('path')
+
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const sess = {
+    secret: 'SECRET',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session(sess))
+
+// Turn on connection to db and server
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now Listening! on ${PORT}`))
+})
+
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
